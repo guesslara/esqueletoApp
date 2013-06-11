b7ca1d573f9b156d591101df907c1bd6b17a97c6
guesslara <glara.styleweb@gmail.com>
2013-06-11 13:43:03 -0500

    Modificacion

--- modulos/funcionesMain.php
+++ modulos/funcionesMain.php
@@ -18,11 +18,64 @@
 	}
 	
 	
-	function guardarInfoBug($mensaje){
+	function guardarInfoBug($mensaje){		
 		$sqlBug="INSERT INTO errores (fecha,hora,des) values ('".date("Y-m-d")."','".date("H:i:s")."','".$mensaje."')";
 		$resBug=mysql_query($sqlBug,conectarBd());
 		if($resBug){
 			echo "<script type='text/javascript'> alert('Informacion enviada Satisfactoriamente.'); cerrarFormbug(); </script>";
+			//se incluye la clase para el envio de correo electronico
+			include("../clases/class.smtp.inc");
+			include("../includes/txtApp.php");
+			$origen_nombre='Desarrollo';
+			$origen_mail="soporte@iqelectronics.com.mx";
+			$password_mail="123456";
+			$subject="Nuevo Bug Enviado";
+			$fecha = date ("d F Y");
+			
+			$params['host'] = 'iqelectronics.com.mx';	// Cambiar por su nombre de dominio
+			$params['port'] = 9025;			// The smtp server port
+			$params['helo'] = 'iqelectronics.com.mx';	// Cambiar por su nombre de dominio
+			$params['auth'] = TRUE;			// Whether to use basic authentication or not
+			$params['user'] = $origen_mail;	// Correo que utilizara para enviar los correos (no usar el de webmaster por seguridad)
+			$params['pass'] = $password_mail;	// Password de la cta de correo. Necesaria para la autenticacion
+			//$destino="glara@iqelectronics.net;uvelez@iqelectronics.net;sistemas@iqelectronics.com.mx;drjuarez@iqelectronics.com.mx;hgmontoya@iqelectronics.com.mx";//$_POST['emailprueba'];consejomexicanodeendodoncia@yahoo.com.mx
+			$destino="glara@iqelectronics.net";//$_POST['emailprueba'];consejomexicanodeendodoncia@yahoo.com.mx
+			$message.='Mensaje Enviado el '.date("d/m/y")." a las ".date("H:i")."<br><br>";
+			$message.='Se ha reportado un BUG en el sistema: '.$txtApp['session']['origenSistemaUsuarioNombre'].'<br><br>';
+			$message.="Descripci&oacute;n<br><br>";
+			$message.='Mensaje: '.$mensaje."<br><br>";
+			$message.="Correo generado Autom&aacute;ticamente<br><br>";
+			$send_params['recipients'] = array("glara@iqelectronics.net"); // The recipients (can be multiple), separados por coma.
+			$send_params['headers']	   = array(
+							'Content-Type: text/html;',
+							'From: "'.$origen_nombre.'" <soporte@iqelectronics.com.mx>',	// Headers
+							//'To: '.$destino,
+							'To: '.$destino,
+							'Subject: '.$subject,
+							//'Disposition-Notification-To: contacto@odontologos.com.mx',
+							//'Disposition-Notification-To: '.$origen_mail,
+							//'Return-Receipt-To: '.$origen_mail,		
+							'Date: '.date(DATE_RFC822),
+							'X-Mailer: PHP/' . phpversion(),
+							'MIME-Version: 1.0',
+							//'Reply-To: '.$origen_mail'\r\n',
+							'Return-Path: '.$origen_nombre.'" <sistema Interno TVO>',
+							'Envelope-To:'.$destino 
+							);
+	
+			$send_params['from']		= $origen_mail;	// This is used as in the MAIL FROM: cmd
+																							
+			$send_params['body']		= $message;	//Message							// The body of the email
+	
+			if(is_object($smtp = smtp::connect($params)) AND $smtp->send($send_params)){
+				echo "";//
+				// Any recipients that failed (relaying denied for example) will be logged in the errors variable.
+				//print_r($smtp->errors);
+			}else {
+				//echo " - NO se envio";
+				//echo "<script type='text/javascript'> alert('".$smtp->errors."'); </script>";
+			}
+			//fin de la inclusion del nuevo codigo de envio de mail
 		}else{
 			echo "Ha ocurrido un error al enviar los datos.";
 		}
